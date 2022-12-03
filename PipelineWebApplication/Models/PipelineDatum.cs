f0d2f6956778c798e74df4b006a33858d7b728a7
Камиль Аль-Айюби <kalajubi@gmail.com>
2022-12-03 14:11:04 +0300

    translate pipidanum

--- PipelineWebApplication/Models/PipelineDatum.cs
+++ PipelineWebApplication/Models/PipelineDatum.cs
@@ -17,52 +17,132 @@ public partial class PipelineDatum
     [Display(Name= "Бригада")]
     public int? BrigadeId { get; set; }
 
+    /// <summary>
+    /// код месторождения
+    /// </summary>
     [Display(Name = "Месторождение")]
     public int? FieldId { get; set; }
 
+    /// <summary>
+    /// код обекта начала и конца
+    /// </summary>
     [Display(Name = "Объект начала")]
     public int? RegionStartId { get; set; }
 
     [Display(Name = "Объект конца")]
     public int? RegionEndId { get; set; }
 
+    /// <summary>
+    /// код контрольного объекта
+    /// </summary>
     [Display(Name = "Контрольный объект")]
     public int? RegionControlId { get; set; }
 
+    /// <summary>
+    /// наименование
+    /// </summary>
+    [Display(Name = "Наименование")]
     public string Name { get; set; } = null!;
 
+    /// <summary>
+    /// признак дубля
+    /// </summary>
+    [Display(Name = "Признак дубля")]
     public int? SignDouble { get; set; }
 
+    /// <summary>
+    /// место врезки
+    /// </summary>
+    [Display(Name = "Место врезки")]
     public string? TieInPlace { get; set; }
 
+    /// <summary>
+    /// длина
+    /// </summary>
+    [Display(Name = "Длмна")]
     public double? Length { get; set; }
 
+    /// <summary>
+    /// оптимизир. длина
+    /// </summary>
+    [Display(Name = "Оптимизированная длина")]
     public double? OptimizedLength { get; set; }
 
+    /// <summary>
+    /// дата ввода в эксплуатацию
+    /// </summary>
+    [Display(Name = "Дата ввода в эксплуатацию")]
     public DateOnly? Date { get; set; }
 
+    /// <summary>
+    /// назначение
+    /// </summary>
+    [Display(Name = "Назначение")]
     public string? Purpose { get; set; }
 
+    /// <summary>
+    /// Транспортир.среда
+    /// </summary>
+    [Display(Name = "Транспортируемая среда")]
     public string? TransportedMedium { get; set; }
 
+    /// <summary>
+    /// категория
+    /// </summary>
+    [Display(Name = "Категория")]
     public char? Category { get; set; }
 
+    /// <summary>
+    /// номер по тех. схеме
+    /// </summary>
+    [Display(Name = "№ по технологической схеме")]
     public string? FlowsheetNumber { get; set; }
 
+    /// <summary>
+    /// Примечание
+    /// </summary>
+    [Display(Name = "Примечание")]
     public string? Note { get; set; }
 
+    /// <summary>
+    /// дебит жидкости
+    /// </summary>
+    [Display(Name = "Дебит жидкости")]
     public double? DebitWater { get; set; }
 
+    /// <summary>
+    /// дебит нефти
+    /// </summary>
+    [Display(Name = "Дебит нефти")]
     public double? DebitOil { get; set; }
 
+    /// <summary>
+    /// Обводненность
+    /// </summary>
+    [Display(Name = "Обводненность")]
     public string? State { get; set; }
 
+    /// <summary>
+    /// температура
+    /// </summary>
+    [Display(Name = "Температура")]
     public double? Temperature { get; set; }
 
+    /// <summary>
+    /// Р факт
+    /// </summary>
+    [Display(Name = "Р фактическое")]
     public double? PFact { get; set; }
 
+    /// <summary>
+    /// Р расчетн
+    /// </summary>
+    [Display(Name = "Р расчетное")]
     public double? PCalculated { get; set; }
 
+    /// <summary>
+    /// Пометка удалено
+    /// </summary>
     public bool IsDeleted { get; set; }
 
     public virtual Brigade? Brigade { get; set; }
