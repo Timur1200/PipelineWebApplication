a8302017cce3c19812cda78c9eb01528e516d31e
Краутер Тимур <xajrullin-02@mail.ru>
2022-12-18 19:19:45 +0300

    Добавил таблицы Role, User

---
+++ PipelineWebApplication/Models/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PipelineWebApplication;
+
+/// <summary>
+/// Пользователь
+/// </summary>
+public partial class User
+{
+    public int Id { get; set; }
+
+    public int? RoleId { get; set; }
+    /// <summary>
+    /// Имя
+    /// </summary>
+    public string? Name { get; set; }
+    /// <summary>
+    /// Фамилия
+    /// </summary>
+    public string? SurName { get; set; }
+    /// <summary>
+    /// Отчество
+    /// </summary>
+    public string? MiddleName { get; set; }
+    /// <summary>
+    /// Логин
+    /// </summary>
+    public string? Login { get; set; }
+    /// <summary>
+    /// Пароль
+    /// </summary>
+    public string? Pass { get; set; }
+    /// <summary>
+    /// Роль
+    /// </summary>
+    public virtual Role? Role { get; set; }
+}
